d22ebf0199730495a4f2fba57c8f2f9e1da773d4
WoeunJung <leena09123@naver.com>
2024-05-14 22:03:50 +0900

    design: make onboarding-user-screen's ui

--- ieum/app/src/main/java/com/jeongg/ieum/presentation/_common/IeumTheme.kt
+++ ieum/app/src/main/java/com/jeongg/ieum/presentation/_common/IeumTheme.kt
@@ -1,6 +1,9 @@
 package com.jeongg.ieum.presentation._common
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyItemScope
@@ -8,6 +11,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.jeongg.ieum.ui.theme.Dimens
 
@@ -23,6 +27,33 @@ fun IeumThemeWithName(
         item { content() }
     }
 
+}
+
+@Composable
+fun OnboardingTheme(
+    text: String = "",
+    buttonText: String = "",
+    onClick: () -> Unit = {},
+    content: @Composable() (LazyItemScope.() -> Unit)
+) {
+    LazyColumn(
+        contentPadding = PaddingValues(Dimens.NormalPadding),
+    ) {
+        item { OnboardingTitle(text) }
+        item { content() }
+        item { LongButton(buttonText, onClick, true) }
+    }
+
+}
+
+@Composable
+private fun OnboardingTitle(text: String) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.titleLarge,
+        modifier = Modifier.fillMaxWidth().padding(top = 30.dp, bottom = 50.dp),
+        textAlign = TextAlign.Center
+    )
 }
 
 @Composable
