b972152fbac46e245e0e8ae64df50fbb87eaf454
WoeunJung <leena09123@naver.com>
2024-05-31 23:23:51 +0900

    feat: api connection with interest for user

--- ieum/app/src/main/java/com/jeongg/ieum/presentation/interest/component/InterestItemList.kt
+++ ieum/app/src/main/java/com/jeongg/ieum/presentation/interest/component/InterestItemList.kt
@@ -9,20 +9,23 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.jeongg.ieum.data.dto.interest.SubTopic
 import com.jeongg.ieum.presentation._common.Divider
 import com.jeongg.ieum.presentation._common.ShortButton
 
 @Composable
 fun InterestItemList(
     mainCategory: String = "",
-    subCategory: List<String> = emptyList()
+    subCategory: List<SubTopic> = emptyList(),
+    onAddClick: (Long) -> Unit,
+    onDeleteClick: (Long) -> Unit
 ) {
     Column {
         MainCategory(mainCategory)
-        SubCategory(subCategory)
+        SubCategory(subCategory, onAddClick, onDeleteClick)
         Divider(Modifier.padding(vertical = 20.dp))
     }
 }
@@ -38,13 +41,27 @@ private fun MainCategory(mainCategory: String) {
 
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
-private fun SubCategory(subCategory: List<String>) {
+private fun SubCategory(
+    subCategory: List<SubTopic>,
+    onAddClick: (Long) -> Unit,
+    onDeleteClick: (Long) -> Unit
+) {
     FlowRow(
         horizontalArrangement = Arrangement.spacedBy(5.dp),
         verticalArrangement = Arrangement.spacedBy(5.dp)
     ) {
         subCategory.forEach { category ->
-            ShortButton(category, {})
+            val isActive = rememberSaveable { mutableStateOf(category.isActive) }
+            ShortButton(
+                text = category.name,
+                isSelected = isActive.value,
+                onClick = {
+                    if (isActive.value) onDeleteClick(category.interestId)
+                    else onAddClick(category.interestId)
+
+                    isActive.value = isActive.value.not()
+                }
+            )
         }
     }
 }
